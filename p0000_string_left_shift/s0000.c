3fddb0793933202349a59d40b1d9509a043f11ac
Lei Tian <leitian.hust@gmail.com>
2014-02-20 12:56:51 -0600

    first problem

---
+++ p0000_string_left_shift/s0000.c
@@ -0,0 +1,3 @@
+// define left shift operation for strings: move characters at the head of a string to the end of string
+// for example: adcdef => cdefab
+// implement a function to do left shift of strings, time complexity must be O(n), and space complexity is O(1).
