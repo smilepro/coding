98c7e7541333bbd42ce6aa478c14df1367871342
Lei Tian <leitian.hust@gmail.com>
2014-02-24 20:35:31 -0600

    naive solution works now

--- p0002_string_containing/s0002.c
+++ p0002_string_containing/s0002.c
@@ -1,21 +1,21 @@
 // Problem 2: http://blog.csdn.net/v_JULY_v/article/details/6347454
 // Problem Statement
-// Assume there are two strings String 1 and String 2 consisting of letters of an alphabet, but the number of letters in B is less than that in A. 
+// Assume there are two strings String 1 and String 2 consisting of letters of an alphabet, but the number of letters in B is less than that in A.
 // Is there a quick way to determine whether all the letters of B are also included in B?
 // Examples:
 // String 1: ABCDEFGHLMNOPQRS
 // String 2: DCGSRQPO
 // Answer is True
 //
-// String 1: ABCDEFGHLMNOPQRS   
+// String 1: ABCDEFGHLMNOPQRS
 // String 2: DCGSRQPZ
 // Answer is False, since Z in String 2 is not included in String 1
 //
 //
 //
-// 
+//
 // Lei Tian, <leitian.hust@gmail.com>
-// 
+//
 
 #include<stdio.h>
 #include<stdint.h>
@@ -27,17 +27,30 @@ static uint32_t naive_containing_test(char *str_a, char *str_b);
 
 int main(void)
 {
-	const char *a_str = "abcdefghijklmn";
-	const char *b_str = "ghijk";
+	char *a_str = "abcdefghijklmn";
+	char *b_str = "ghijk";
 	uint32_t ret_code;
 
 	ret_code = naive_containing_test(a_str, b_str);
-	printf("A: %s, B: %s, INCLUDED %s\n", a_str, b_str, (re_code==0) ? "False" : "True");
+	printf("A: %s, B: %s, INCLUDED: %s\n", a_str, b_str, (ret_code==0) ? "False" : "True");
 
 	return 0;
 }
 
 static uint32_t naive_containing_test(char *str_a, char *str_b)
 {
-	return 0;
+    int i, j;
+
+    for (i = 0; i < strlen(str_b); i++){
+        for(j = 0; j < strlen(str_a); j++){
+            if (str_b[i] != str_a[j])
+                continue;
+            else
+                break;
+        }
+        if (j >= strlen(str_a))
+            return 0;
+    }
+
+	return 1;
 }
